3e64bfcdb0538753504bd1dc344d21a4be09fad4
Kamalpreet Mundi <kamalmundi31@gmail.com>
2021-10-23 19:02:31 -0600

    Updated Arrows, Added single animation as a test

--- MobileTesting/MobileTesting/MainPage.xaml.cs
+++ MobileTesting/MobileTesting/MainPage.xaml.cs
@@ -18,7 +18,20 @@ namespace MobileTesting
         public void ArrowPressed(object sender, EventArgs e)
         {
             ImageButton thisButton = sender as ImageButton;
+            Console.WriteLine(thisButton.ClassId);
             thisButton.Opacity = 0.5;
+            if (thisButton.ClassId.Equals("Right1"))
+            {
+                //Console.WriteLine("R");
+                Image1.TranslateTo(1000, Image1.Y, 600, Easing.CubicInOut); // With Animation
+  
+            }
+            else if (thisButton.ClassId.Equals("Left1"))
+            {
+                //Console.WriteLine("RR");
+                Image1.TranslateTo(0, Image1.Y, 600, Easing.CubicInOut); // Goes straight no animation
+            }
+            
         }
 
         public void ArrowReleased(object sender, EventArgs e)
